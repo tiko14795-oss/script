1220554f7bb2506a331cc7e3a870c8c5eddc4734
tiko14795-oss <tiko14795@gmail.com>
2026-03-20 19:35:04 +0300

    moy pirviy komit!!!

---
+++ Hello World.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("привет, мир!");
+        print("19.02.14");
+        print("y meny dr");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
